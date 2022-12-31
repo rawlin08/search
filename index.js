e7defbb73345a40bc885a34fbe55f987d33e5cfc
Alec Rawlins <alec12999@yahoo.com>
2022-12-30 23:05:57 -0500

    added an all search function

--- index.js
+++ index.js
@@ -12,37 +12,44 @@ formDetails.addEventListener('submit', search)
 function search(e) {
     e.preventDefault();
     const form = e.target;
-    console.log(form);
-    console.log(form.elements.search.value);
-    console.log(form.elements.language.value);
-    if(form.elements.search.value.length == 0) {
-        return
-    }
-    else if (form.elements.language.value == 'All') {
-        fetch('http://localhost:3000/db')
-            .then((response) => response.json())
-            .then((data) => {
-                propertiesArea.innerHTML = '';
-                let searchedHTML = data.HTML.filter(element => element.property.includes(form.elements.search.value));
-                let searchedCSS = data.CSS.filter(element => element.property.includes(form.elements.search.value));
-                let searchedJS = data.JavaScript.filter(element => element.property.includes(form.elements.search.value));
-                let searched = searchedHTML.concat(searchedCSS, searchedJS);
-                searched.forEach(element => makeNewCard(element));
-            })
-    }
-    else {
-        fetch(`http://localhost:3000/${form.elements.language.value}`)
-            .then((response) => response.json())
-            .then((data) => {
-                propertiesArea.innerHTML = '';
-                let searched = data.filter(element => element.property.includes(form.elements.search.value));
-                searched.forEach(element => makeNewCard(element));
-            })
+    let searchedText = form.elements.search.value.toLowerCase();
+    if(searchedText.length != 0) {
+        if (form.elements.language.value == 'All') {
+            fetch('http://localhost:3000/db')
+                .then((response) => response.json())
+                .then((data) => {
+                    propertiesArea.innerHTML = '';
+                    if (searchedText == 'all') {
+                        data.HTML.forEach(element => makeNewCard(element));
+                        data.CSS.forEach(element => makeNewCard(element));
+                        data.JavaScript.forEach(element => makeNewCard(element));
+                    }
+                    else {
+                        let searchedHTML = data.HTML.filter(element => element.property.includes(searchedText));
+                        let searchedCSS = data.CSS.filter(element => element.property.includes(searchedText));
+                        let searchedJS = data.JavaScript.filter(element => element.property.includes(searchedText));
+                        let searched = searchedHTML.concat(searchedCSS, searchedJS);
+                        searched.forEach(element => makeNewCard(element));
+                    }
+                })
+        }
+        else {
+            fetch(`http://localhost:3000/${form.elements.language.value}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    propertiesArea.innerHTML = '';
+                    if (searchedText == 'all') {
+                        data.forEach(element => makeNewCard(element));
+                    }
+                    let searched = data.filter(element => element.property.includes(searchedText));
+                    searched.forEach(element => makeNewCard(element));
+                })
+        }
+        console.log('Search Done!');
     }
 }
 
 function makeNewCard(element) {
-    console.log(element);
     propertiesArea.innerHTML += 
     `
     <div class="card">
